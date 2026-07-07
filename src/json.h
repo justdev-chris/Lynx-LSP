078bbe0ea43a8e455c1bd5e4640665b38e4c2332
Chris <morenochristopher580@gmail.com>
2026-07-07 11:29:30 -0600

    Create json.h

---
+++ src/json.h
@@ -0,0 +1,34 @@
+#ifndef JSON_H
+#define JSON_H
+
+typedef enum {
+    JSON_NULL,
+    JSON_BOOL,
+    JSON_NUMBER,
+    JSON_STRING,
+    JSON_ARRAY,
+    JSON_OBJECT
+} json_type;
+
+typedef struct json_value {
+    json_type type;
+    union {
+        int boolean;
+        double number;
+        char* string;
+        struct json_value** array;
+        struct json_pair* object;
+    };
+    int length;
+} json_value;
+
+typedef struct json_pair {
+    char* key;
+    json_value* value;
+} json_pair;
+
+json_value* json_parse(const char* str);
+json_value* json_get_object(json_value* obj, const char* key);
+void json_free(json_value* val);
+
+#endif
