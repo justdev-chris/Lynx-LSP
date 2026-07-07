0b721d743fb4d8e1e58c41451984d0f77ac44e83
Chris <morenochristopher580@gmail.com>
2026-07-07 11:27:53 -0600

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "lsp.h"
+
+#define BUFFER_SIZE 65536
+
+int main(int argc, char* argv[]) {
+    if (argc > 1 && strcmp(argv[1], "--help") == 0) {
+        printf("🐱 Lynx-LSP v0.1.0\n");
+        printf("Language Server Protocol for Lynx\n\n");
+        printf("Usage:\n");
+        printf("  lynx-lsp --stdio    Run as LSP server\n");
+        printf("  lynx-lsp --version  Show version\n");
+        printf("  lynx-lsp --help     Show this help\n");
+        return 0;
+    }
+
+    if (argc > 1 && strcmp(argv[1], "--version") == 0) {
+        printf("Lynx-LSP 0.1.0\n");
+        return 0;
+    }
+
+    if (argc > 1 && strcmp(argv[1], "--stdio") == 0) {
+        lsp_server_run();
+        return 0;
+    }
+
+    printf("🐱 Lynx-LSP v0.1.0\n");
+    printf("Run with --stdio to start LSP server, or --help for usage.\n");
+    return 0;
+}
